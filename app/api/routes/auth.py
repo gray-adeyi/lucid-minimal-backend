60928f85e757eaf09590651a702eab273301464d
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-04-01 20:00:49 +0100

    Raise error for unimplemented endpoints

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
 
-auth_router =  APIRouter(prefix="/auth",tags=["User Authentication"])
+from app.schema import SignUpSchema, SignInSchema
+
+auth_router = APIRouter(prefix="/auth", tags=["User Authentication"])
 
 
-@auth_router.post('/sign-up')
-async def sign_up():
-    ...
+@auth_router.post("/sign-up")
+async def sign_up(body: SignUpSchema):
+    raise NotImplementedError("This endpoint is yet to  be implemented")
 
-@auth_router.post('/login')
-async def login():
-    ...
+
+@auth_router.post("/login")
+async def login(body: SignInSchema):
+    raise NotImplementedError("This endpoint is yet to  be implemented")
--- app/api/routes/posts.py
+++ app/api/routes/posts.py
@@ -2,19 +2,24 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
-posts_router =  APIRouter(prefix="/posts",tags=["Posts"])
+posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 
-@posts_router.post('/')
+
+@posts_router.post("/")
 async def create_post():
-    ...
-@posts_router.get('/')
+    raise NotImplementedError("This endpoint is yet to  be implemented")
+
+
+@posts_router.get("/")
 async def get_posts():
-    ...
+    raise NotImplementedError("This endpoint is yet to  be implemented")
 
-@posts_router.get('/{id}/')
+
+@posts_router.get("/{id}/")
 async def get_post_detail(id: UUID):
-    ...
+    raise NotImplementedError("This endpoint is yet to  be implemented")
 
-@posts_router.delete('/{id}/')
+
+@posts_router.delete("/{id}/")
 async def delete_post(id: UUID):
-    ...
+    raise NotImplementedError("This endpoint is yet to  be implemented")
