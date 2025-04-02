d0909c34af88c260106a2755a3d82c2b97dfa5c8
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-04-02 01:22:04 +0100

    Implement auth endpoints

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,15 +1,27 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from app.schema import SignUpSchema, SignInSchema
+from fastapi import APIRouter, Depends
+
+from app.api.deps import get_user_from_form_data, create_user_from_signup
+from app.schema import TokenSchema, ResponseSchema, SignUpSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["User Authentication"])
 
 
 @auth_router.post("/sign-up")
-async def sign_up(body: SignUpSchema):
-    raise NotImplementedError("This endpoint is yet to  be implemented")
+async def sign_up(
+    body: SignUpSchema,
+    tokens: Annotated[
+        TokenSchema, Depends(TokenSchema.generate_tokens(create_user_from_signup))
+    ],
+):
+    return ResponseSchema(data=tokens)
 
 
 @auth_router.post("/login")
-async def login(body: SignInSchema):
-    raise NotImplementedError("This endpoint is yet to  be implemented")
+async def login(
+    tokens: Annotated[
+        TokenSchema, Depends(TokenSchema.generate_tokens(get_user_from_form_data))
+    ],
+):
+    return ResponseSchema(data=tokens)
