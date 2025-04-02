d223e5c99fb46fa9b78c26bb64df94a45ede2d18
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-04-02 01:23:56 +0100

    Fix incorrect relationship

--- app/models.py
+++ app/models.py
@@ -1,24 +1,23 @@
 # models.py
-from sqlalchemy import Column, Integer, String, DateTime,text,ForeignKey
-from sqlalchemy.orm import declarative_base, relationship,declared_attr
+from uuid import uuid4
+
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, declared_attr
 from sqlalchemy.sql import func
 
 
 class Base:
     @declared_attr
     def __tablename__(cls):
-        return f'{cls.__name__.lower()}s'
+        return f"{cls.__name__.lower()}s"
 
-    id = Column(
-        String(36),
-        primary_key=True,
-        server_default=text("UUID()")
-    )
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid4()))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
 
 BaseDBModel = declarative_base(cls=Base)
+
 
 class User(BaseDBModel):
     __tablename__ = "users"
@@ -29,11 +28,11 @@ class User(BaseDBModel):
     # Relationship
     posts = relationship("Post", back_populates="author")
 
+
 class Post(BaseDBModel):
     __tablename__ = "posts"
 
-    author_id = Column(String(36),  ForeignKey('users.id'))
+    author_id = Column(String(36), ForeignKey("users.id"))
     text = Column(String(1000), nullable=False)
 
-    posts = relationship("User", back_populates="posts")
-
+    author = relationship("User", back_populates="posts")
