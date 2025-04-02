a5ab2a5c152f65ae26a162d3739247492dbc27c0
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-04-02 01:26:56 +0100

    Protect all post endpoints

--- app/api/routes/posts.py
+++ app/api/routes/posts.py
@@ -2,24 +2,36 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
+from app.api.deps import AuthenticatedUserDep
+
 posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 @posts_router.post("/")
-async def create_post():
+async def create_post(
+    user: AuthenticatedUserDep,  # noqa: ARG001 ignore unused arg, it is used to protect route
+):
     raise NotImplementedError("This endpoint is yet to  be implemented")
 
 
 @posts_router.get("/")
-async def get_posts():
+async def get_posts(
+    user: AuthenticatedUserDep,  # noqa: ARG001 ignore unused arg, it is used to protect route
+):
     raise NotImplementedError("This endpoint is yet to  be implemented")
 
 
 @posts_router.get("/{id}/")
-async def get_post_detail(id: UUID):
+async def get_post_detail(
+    id: UUID,
+    user: AuthenticatedUserDep,  # noqa: ARG001 ignore unused arg, it is used to protect route
+):
     raise NotImplementedError("This endpoint is yet to  be implemented")
 
 
 @posts_router.delete("/{id}/")
-async def delete_post(id: UUID):
+async def delete_post(
+    id: UUID,
+    user: AuthenticatedUserDep,  # noqa: ARG001 ignore unused arg, it is used to protect route
+):
     raise NotImplementedError("This endpoint is yet to  be implemented")
